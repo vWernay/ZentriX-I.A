3b5448deaa88c6776c851309cb7f40fefa101e3c
victorBOY <32583593+vWernay@users.noreply.github.com>
2020-03-24 17:32:35 -0300

    REMOVIDO BOLSONARO MEME

--- cmds/meme.js
+++ cmds/meme.js
@@ -8,7 +8,7 @@ module.exports = {
     run: async (client, message, args) => {
         // In this array, 
         // you can put the subreddits you want to grab memes from
-        const subReddits = ["brasil", "brasil_drama", "brasilivre", "bolsonaro"];
+        const subReddits = ["brasil", "brasil_drama", "brasilivre"];
         // Grab a random property from the array
         const random = subReddits[Math.floor(Math.random() * subReddits.length)];
 
