411bcc015b334d7c407ab9098a7d1cbd3879be71
victorBOY <32583593+vWernay@users.noreply.github.com>
2020-04-06 17:20:39 -0300

    CORRIGIDO O BUG DE CANAL

--- fivem.js
+++ fivem.js
@@ -262,7 +262,7 @@ client.on("message", async message => {
 
 client.on("message", msg => {
   
-  if (msg.author.bot) return;
+  if (msg.author.bot || msg.channel.id == '612650778871595074') return;
 
   let wordArray = msg.content.split(" ");
   //console.log(wordArray);
