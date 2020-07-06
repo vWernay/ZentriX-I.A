9607497d94a97396e9f52693ed682f442a1c0dc2
victorBOY <32583593+vWernay@users.noreply.github.com>
2020-07-06 17:01:39 -0300

    Atualização

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,29 +1,19 @@
-const​ ​Discord​ ​=​ ​require​(​"​discord.js​"​);
-
 module.exports = {
- //var util = require("../fivem");
-    // Edit the message
-    name: "avatar",
-    aliases: ["av"],
-    category: "fun",
-    description: "Pega o avatar da pessoa mencionada.",
-    usage: "[menção]",
-    run: async (client, message, args) => {
+    name: 'avatar',
+    aliases: ['icon', 'logo'],
+    description: 'display the image and url of users\' avatar',
+    usage: '[tagged users]',
+    execute(client, message, args) {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
+        }
 
-    ​let​ msg ​=​ ​await​ ​message​.​channel​.​send​(​"​Gerando avatar...​"​);
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username}'s avatar: ${user.displayAvatarURL}`;
+        });
 
-    ​let​ mentionedUser ​=​ ​message​.​mentions​.​users​.​first​() ​||​ ​message​.​author​;
-
-        ​let​ embed ​=​ ​new​ ​Discord.RichEmbed​()
-
-        .​setImage​(​mentionedUser​.​displayAvatarURL​)
-        .​setColor​(​"​00ff00​"​)
-        .​setTitle​(​"​Avatar​"​)
-        .​setFooter​(​"​Requerido por ​"​ ​+​ ​message​.​author​.​tag​)
-        .​setDescription​(​"​[Avatar URL link](​"​+​mentionedUser​.​displayAvatarURL​+​"​)​"​);
-
-        ​message​.​channel​.​send​(embed)
-
-
-    ​msg​.​delete​();
-}
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
+    },
+};
