ef2afa2463f4263363f98e1f2fc08f96b8de3539
victorBOY <32583593+vWernay@users.noreply.github.com>
2020-07-06 21:50:10 -0300

    Novos comandos

---
+++ cmds/aviso.js
@@ -0,0 +1,34 @@
+
+    const Discord = require('discord.js')
+    const c = require('../cmds/config/config.json')
+    exports.run = async (client, message, args) => {
+        await message.delete()
+        if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${message.author}, você não possui permissão para executar esse comando.`).then(msg => msg.delete(8000))
+
+        let mensg = args.join(' ')
+        if (!mensg) {
+            message.channel.send(`${message.author}, digite uma mensagem para anunciar. :mailbox_with_no_mail:`)
+            return undefined;
+        }
+
+        const embed = new Discord.RichEmbed()
+            .setAuthor('ZentriX RP - Anúncio', client.user.avatarURL)
+            .setDescription(`${mensg}
+        
+    Atenciosamente,
+    Equipe **ZentriX RP**.`)
+            .setColor(c.cor)
+            .setThumbnail(client.user.avatarURL)
+            .setTimestamp()
+            .setFooter(`Publicado por: ${message.author.username}`, message.author.avatarURL)
+        client.channels.get(c.newsChannel).send('a').then(msg => msg.delete(2000))
+        client.channels.get(c.newsChannel).send(embed)
+    }
+
+    exports.help = {
+        name: "aviso",
+        aliases: [
+            "anuncio",
+            "anunciar"
+        ]
+    }
---
+++ cmds/quiz.js
@@ -0,0 +1,115 @@
+const Discord = require('discord.js');
+
+const items = [{
+        question: 'Quais foram os criadores do Servidor?',
+        answer: ['Aprendiz', 'victorBOY'],
+    },
+    {
+        question: 'Qual foi o primeiro nome do servidor?',
+        answer: ['Arkham City'],
+    },
+    {
+        question: 'Quando o Servidor foi criado?',
+        answer: ['13 de Agosto de 2019'],
+    },
+];
+
+const config = {
+    max: 1,
+    time: 60000,
+    errors: ['time'],
+};
+
+const random = items[Math.floor(Math.random() * items.length)];
+
+exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Você não tem permissão para usar este comando`);
+
+    let tempo = 0;
+
+    const addTime = setInterval(() => {
+        tempo += 1;
+    }, 1000);
+
+    // Manda o tempo do jogo, a cada 30 seg
+    const sendTime = setInterval(() => {
+        message.channel.send(
+            `⌚ O jogo está rolando! Já se passaram ${tempo + 1} segundos`
+        );
+    }, 30000);
+
+    const question = new Discord.RichEmbed()
+        .setTitle(`Evento QUIZ\n**${random.question}**`)
+        .setDescription(
+            'Lembre-se que todas as respostas estão em português e somente será válida a que estiver escrita corretamente.'
+        )
+        .setColor('RANDOM');
+
+    message.channel.send('Irá começar em :three: segundos...');
+    setTimeout(() => {
+        message.channel.send('Irá começar em :two: segundos...');
+    }, 1000);
+    setTimeout(() => {
+        message.channel.send('Irá começar em :one: segundos...');
+    }, 2000);
+    setTimeout(() => {
+        message.channel.send(question);
+    }, 3000);
+
+    try {
+        const collector = await message.channel.awaitMessages(
+            answer => random.answer.includes(answer.content.toLowerCase()),
+            config
+        );
+        const winnerAnswer = collector.first();
+        winnerAnswer.delete();
+
+        const win = new Discord.RichEmbed()
+            .setTitle(
+                `Evento QUIZ\n**${
+						winnerAnswer.author.username
+					} acertou.**`
+            )
+            .setDescription(
+                `**Informações:**\n Ganhador: ${
+						winnerAnswer.author.username
+					}\n Pergunta: \`\`${
+						random.question
+					}\`\`\n Premiação: \`\`xxx\`\`.` +
+                `\n Tempo de jogo: \`\`${tempo} segundos\`\`.`
+            )
+            .setColor('RANDOM');
+
+        message.channel.send(win);
+        client.channels.get().send(win);
+        clearInterval(addTime);
+        return clearInterval(sendTime);
+    } catch (e) {
+        console.log('enviou porra', e);
+        // if(e.message == 'time') {
+        const timeout = new Discord.RichEmbed()
+            .setTitle(
+                'Evento QUIZ\n**' +
+                'Quiz finalizado pois não houve acertos.' +
+                '**'
+            )
+            .setDescription(
+                `${'**Informações:**\n Ganhador: ' +
+						'Ninguém' +
+						'\n Pergunta: ``'}${
+						random.question
+					}\`\`\n Premiação: \`\`xxx\`\`.` +
+                `\n Tempo de jogo: \`\`${tempo} segundos\`\`.`
+            )
+            .setColor('RANDOM');
+        message.channel.send(timeout);
+        clearInterval(addTime);
+        return clearInterval(sendTime);
+        // }
+    }
+}
+
+
+exports.help = {
+    name: 'quiz'
+}
